23e2c7af42651d1c98e6f112a51c52d3c3ee35f8
alexey_kozyakov <xelexerx@gmail.com>
2026-09-02 18:10:45 +0700

    Revert releases 1.1, 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alexey.kozyakov.snake"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
